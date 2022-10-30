27e9e6ecf653fba738477912fe51ad1b26cc80c2
nexxyb <nexxyb@gmail.com>
2022-10-30 12:33:57 +0100

    Third commit

---
+++ palindrome.py
@@ -0,0 +1,19 @@
+def reverse_list(s:list)-> list:
+    """function the returns reverse of a list"""
+    if len(s)==2:            
+        return [s[1],s[0]]
+    elif len(s)>2:
+        return [s[-1]]+reverse_list(s[:-1])
+def isPalindrome( head) -> bool:
+    if len(head)<=2:
+        return 'false'
+    elif len(head) > 2:
+        reverse= reverse_list(head)
+        if head==reverse:
+            return 'true'
+        else:
+            return 'false'
+print(isPalindrome(head=[1,2,2,1]))
+print(isPalindrome(head=[1,2,3,2,1,4,5,6,7,8,9,0,2,1,1,3,4,5,6,7,7,88,8,]))
+print(isPalindrome(head=[m for m in range(0,149)]))
+print(isPalindrome(head=[1,2,5,6,5,2,1]))
